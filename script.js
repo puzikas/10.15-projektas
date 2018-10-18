52aa2d1f93ee5c181d14ae2e911dde6c04bf83a5
Lukas <lukas.chrap@gmail.com>
2018-10-18 21:26:16 +0300

    10.18

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ var rim = document.getElementById('rim');
 var size = document.getElementById('size');
 var result = document.getElementById('result');
 
+
 function calculate() {
 	var a = parseInt(tyre.value) / 10;
 	var b = parseInt(rim.value) * 2.54;
 	var c = parseInt(size.value) * 2;
-
+	var d = (a + b) / 2;
 	if (a + b <= c) {
 		console.log('ratai tilps');
+		result.innerHTML = 'Ratas tilps'
 
 	} else {
 		console.log('ratai netilps');
-		alert("Toks derinys netinkamas, pasirinkite mažesnį ratą arba padangą.")
+		alert('Ratas yra per didelis!')
+		result.innerHTML = 'Toks derinys netinkamas, pasirinkite mažesnį ratą arba padangą. Minimalus arkos spindulys su šiuo deriniu - ' + d +'cm'
 	}
-
-	result = a + b;
-
 }
